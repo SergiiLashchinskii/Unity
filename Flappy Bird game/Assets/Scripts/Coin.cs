c9ff47b59bc86bae444429309ea10b63ac590bfd
SergiiLashchinskii <SergiiLashchinskii@users.noreply.github.com>
2020-06-03 12:53:13 +0300

    Добавление монет

---
+++ Flappy Bird game/Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
