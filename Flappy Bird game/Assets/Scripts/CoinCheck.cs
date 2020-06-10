4d95a13133bb3600da99facc18ada98356c7beea
SergiiLashchinskii <SergiiLashchinskii@users.noreply.github.com>
2020-06-11 00:38:57 +0300

    Фикс

---
+++ Flappy Bird game/Assets/Scripts/CoinCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCheck : MonoBehaviour
+{
+    void Start()
+    {
+        CoinCongrats.SetActive(false);
+    }
+    public GameObject CoinCongrats;
+    void Update()
+    {
+        if (!GameObject.FindWithTag("Coin"))
+        {
+            CoinCongrats.SetActive(true);
+        }
+    }
+}
